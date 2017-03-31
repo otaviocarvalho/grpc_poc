9b1bfa87eec7110d78107794f4b6e799f32979be
otaviocarvalho <ocarvalh@thoughtworks.com>
2017-03-31 08:07:25 -0300

    Init grpc to get customers from server

---
+++ server/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+	"net"
+	"strings"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	pb "grpc_poc/customer"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct {
+	savedCustomers []*pb.CustomerRequest
+}
+
+func (s *server) CreateCustomer(ctx context.Context, in *pb.CustomerRequest) (*pb.CustomerResponse, error) {
+	s.savedCustomers = append(s.savedCustomers, in)
+	return &pb.CustomerResponse{Id: in.Id, Success: true}, nil
+}
+
+func (s *server) GetCustomers(filter *pb.CustomerFilter, stream pb.Customer_GetCustomersServer) error {
+	for _, customer := range s.savedCustomers {
+		if filter.Keyword != "" {
+			if !strings.Contains(customer.Name, filter.Keyword) {
+				continue
+			}
+		}
+		if err := stream.Send(customer); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterCustomerServer(s, &server{})
+	s.Serve(lis)
+}
