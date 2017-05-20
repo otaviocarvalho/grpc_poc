1ccf22a4da167f60eb5d6756209b57ca92ebcdff
otaviocarvalho <otaviio@gmail.com>
2017-05-20 14:36:11 +0000

    Include install_dependencies bash script

---
+++ install_dependencies.sh
@@ -0,0 +1,8 @@
+# Install docker
+sudo apt-get update
+sudo apt-get install docker
+
+# Install golang and dependencies
+go get google.golang.org/grpc
+go get -u github.com/golang/protobuf/proto
+go get -u github.com/golang/protobuf/protoc-gen-go
