988a0ec063d01bb6b83920ba0f3989a4447cd0a1
otaviocarvalho <ocarvalh@thoughtworks.com>
2017-05-30 21:25:34 -0300

    Adds HdrHistogram to ping/pong to proper measure latency

--- client/ping.go
+++ client/ping.go
@@ -3,18 +3,21 @@ package main
 import (
 	"log"
     "time"
-    "math"
-    "fmt"
+    //"math"
+    //"fmt"
     "crypto/rand"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+    //"github.com/codahale/hdrhistogram"
 
 	pb "grpc_poc/protobuf"
+	hdr "github.com/otaviocarvalho/hdrhistogram"
 )
 
 const (
-	address = "localhost:50051"
+    //address = "191.232.175.110:50051"
+    address = "localhost:50051"
 )
 
 func sendPing(client pb.PingPongClient, ping *pb.Ping) {
@@ -36,7 +39,7 @@ func main() {
 	// Creates a new Ping
 	client := pb.NewPingPongClient(conn)
 
-    msgSize := 100
+    msgSize := 64 * 1000
     randomBytes := make([]byte, msgSize)
     _, err = rand.Read(randomBytes)
     if err != nil {
@@ -48,24 +51,34 @@ func main() {
         Payload: msg,
     }
 
-    timeBucket := make(map[string]float64)
+    // 1ms to 30 seconds range, 5 sigfigs precision
+    hist := hdr.New(1000000, 30000000000, 5)
 
-    numRuns := 100
+    numRuns := 10
+    startTime := time.Now()
     for i := 0; i < numRuns; i++ {
-        startTime := time.Now()
+        startTimeLoop := time.Now()
 
         // Sends ping
         sendPing(client, ping)
 
-        totalTime := time.Now().Sub(startTime)
-        fmt.Printf("Cur Latency\t%v\n", time.Duration(totalTime))
-        timeBucket["sum"] += float64(totalTime)
-        timeBucket["minLatency"] = math.Min(float64(totalTime), float64(timeBucket["minLatency"]))
-        timeBucket["maxLatency"] = math.Max(float64(totalTime), float64(timeBucket["maxLatency"]))
+        totalTimeLoop := time.Now().Sub(startTimeLoop)
+        hist.RecordValue(totalTimeLoop.Nanoseconds())
     }
 
-    fmt.Printf("Total time\t%v\n", time.Duration(timeBucket["sum"]))
-    fmt.Printf("Min Latency\t%v\n", time.Duration(timeBucket["minLatency"]))
-    fmt.Printf("Max Latency\t%v\n", time.Duration(timeBucket["maxLatency"]))
-    fmt.Printf("Average latency\t%v\n", time.Duration(timeBucket["sum"] / float64(numRuns)))
+    totalTime := time.Now().Sub(startTime)
+    log.Printf("QPS: ", float64(numRuns) / totalTime.Seconds())
+
+    log.Printf("50th: %d\n", hist.ValueAtQuantile(50))
+    log.Printf("90th: %d\n", hist.ValueAtQuantile(90))
+    log.Printf("99th: %d\n", hist.ValueAtQuantile(99))
+    log.Printf("99.9th: %d\n", hist.ValueAtQuantile(99.9))
+    log.Printf("99.99th: %d\n", hist.ValueAtQuantile(99.99))
+    log.Printf("99.999th: %d\n", hist.ValueAtQuantile(99.999))
+    log.Printf("99.9999th: %d\n", hist.ValueAtQuantile(99.9999))
+    log.Printf("99.99999th: %d\n", hist.ValueAtQuantile(99.99999))
+    log.Printf("99.999999th: %d\n", hist.ValueAtQuantile(99.999999))
+    log.Printf("99.9999999th: %d\n", hist.ValueAtQuantile(99.9999999))
+    log.Printf("99.99999999th: %d\n", hist.ValueAtQuantile(99.99999999))
+    log.Printf("99.999999999th: %d\n", hist.ValueAtQuantile(99.999999999))
 }
