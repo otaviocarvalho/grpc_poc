09b57ed30ba91d9eb70f36a7932d6df89488510a
otaviocarvalho <ocarvalh@thoughtworks.com>
2017-07-05 21:02:12 -0300

    Include hdrhistogram into dependencies script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,3 +6,4 @@ sudo apt-get install docker
 go get google.golang.org/grpc
 go get -u github.com/golang/protobuf/proto
 go get -u github.com/golang/protobuf/protoc-gen-go
+go get github.com/otaviocarvalho/hdrhistogram
