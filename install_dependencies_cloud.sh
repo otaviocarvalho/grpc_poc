05f48320d4fa78a46595b5662516e13a6fe44b24
root <root@iot-gppd-otavio-auto-edge-1.jiwm52h3djzehif5isalffh5cd.nx.internal.cloudapp.net>
2017-11-06 03:15:27 +0000

    Adjust weird boot issue by waiting for dpkg to release lock

--- install_dependencies_cloud.sh
+++ install_dependencies_cloud.sh
@@ -3,10 +3,13 @@ cd /home/ocarval/
 
 # Install golang
 mkdir /home/ocarval/go
+time (while ps -opid= -C apt-get > /dev/null; do sleep 1; done); # wait for debian update to release dpkg lock
 sudo apt-get install golang-go -y
 echo "export GOPATH=/home/ocarval/go" >> /home/ocarval/.bash_aliases
 echo "export PATH=$PATH:$GOROOT/bin:$GOPATH/bin" >> /home/ocarval/.bash_aliases
 source /home/ocarval/.bash_aliases
+sudo chown -R ocarval.ocarval *
+sudo chown -R ocarval.ocarval .*
 
 # Install itself as a dependency 
 go get -u github.com/otaviocarvalho/grpc_poc
