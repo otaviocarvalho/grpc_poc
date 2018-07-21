71270df04109ea0c4bc7f52d8be7025489f4693d
otaviocarvalho <ocarvalh@thoughtworks.com>
2018-07-21 04:12:11 -0300

    Add QPS logging to server

--- iot/server/iot_server.go
+++ iot/server/iot_server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"flag"
 	"log"
 	"math"
@@ -8,6 +9,8 @@ import (
 	"sync"
 	"sync/atomic"
 	"time"
+	"encoding/json"
+	"io/ioutil"
 
 	"go4.org/net/throttle"
 	"golang.org/x/net/context"
@@ -29,8 +32,13 @@ var expMAvgMutex sync.RWMutex
 var counterMutex sync.Mutex
 
 var port = flag.String("p", ":50052", "ip/port")
-
 var latency = flag.Duration("l", 0*time.Millisecond, "artificial latency")
+var outputFile = flag.String("o", "./output_stats_server.json", "output json file")
+var batchLogSize = flag.Int64("blog", 1000, "batch size for logging")
+
+var messageChannel = make(chan int64, 100000000)
+
+var startTime time.Time
 
 func (s *dataServer) SendMeasurement(ctx context.Context, in *pb.Measurement) (*pb.Measurement, error) {
 	// Calculate EWMA
@@ -42,6 +50,7 @@ func (s *dataServer) SendMeasurement(ctx context.Context, in *pb.Measurement) (*
 	counterMutex.Lock()
 	atomic.AddInt64(&counter, 1)
 	curCount := counter
+	messageChannel <- curCount
 	counterMutex.Unlock()
 
 	expMAvgMutex.RLock()
@@ -51,8 +60,38 @@ func (s *dataServer) SendMeasurement(ctx context.Context, in *pb.Measurement) (*
 	return measurement, nil
 }
 
+type stats struct {
+	QPS	float64 `json:"qps"` // Should multiply this number by the batch size on the aggregator side
+}
+
+func saveStats(numRuns int64, totalTime time.Duration) {
+	stats := stats{
+		float64(numRuns) / totalTime.Seconds(),
+	}
+
+	statsJSON, _ := json.Marshal(stats)
+	err := ioutil.WriteFile(*outputFile, statsJSON, 0664)
+	if err != nil {
+		fmt.Println("Writing output file", err.Error())
+	}
+}
+
 func main() {
 	flag.Parse()
+
+	// Log summary each couple of seconds
+	startTime = time.Now()
+	go func() {
+		for {
+			select {
+				case curCount := <-messageChannel:
+					fmt.Println(curCount)
+					if (curCount % *batchLogSize == 0) {
+						saveStats(curCount, time.Now().Sub(startTime))
+					}
+			}
+		}	
+	}()
 
 	lis, err := net.Listen("tcp", *port)
 	if err != nil {
